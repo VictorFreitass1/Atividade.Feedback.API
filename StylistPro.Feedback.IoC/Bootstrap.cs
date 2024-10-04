3c42250172b456792f228b29edfb2e0632349f1b
VICTOR FREITAS SILVA <victor.fsilva45@gmail.com>
2024-10-04 16:31:19 -0300

    Add project files.

---
+++ StylistPro.Feedback.IoC/Bootstrap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StylistPro.Feedback.Application.Services;
+using StylistPro.Feedback.Data.AppData;
+using StylistPro.Feedback.Data.Repositories;
+using StylistPro.Feedback.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace StylistPro.Feedback.IoC
+{
+    public static class Bootstrap
+    {
+        public static void Start(IServiceCollection services, IConfiguration configuration)
+            
+        {
+            services.AddDbContext<ApplicationContext>(options => {
+                options.UseOracle(configuration["ConnectionStrings:Oracle"]);
+            });
+
+            services.AddTransient<IFeedbackRepository, FeedbackRepository>();
+            services.AddTransient<IFeedbackApplicationService, FeedbackApplicationService>();
+        }
+    }
+}
